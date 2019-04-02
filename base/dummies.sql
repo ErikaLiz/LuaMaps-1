70b1b02d908e914f0dcc755fa4ee499643a3ac2a
Cesar Lara <larachicharo@protonmail.com>
2019-04-02 13:37:29 -0500

    Creating dummies.sql

---
+++ base/dummies.sql
@@ -0,0 +1,8 @@
+INSERT INTO usuario (correo, nombreusuario, rol, contrasenia)
+VALUES ('dummy@dummy.com', 'dummy', 1, 'dummy');
+
+INSERT INTO tema (nombretema, color, correo)
+VALUES ('dummytema', 'azul', 'dummy@dummy.com');
+
+INSERT INTO marcador (descripcion, latitud, longitud, idtema)
+VALUES ('Marcador dummy', 59.89, 30.26, 1);
