b36489fcec58f2357a437242584ba0ab7fabf094
Cesar Lara <larachicharo@protonmail.com>
2019-04-05 10:08:32 -0500

    Changing dummies.sql according to current databse.

--- base/dummies.sql
+++ base/dummies.sql
@@ -1,8 +1,8 @@
 INSERT INTO usuario (correo, nombreusuario, rol, contrasenia)
-VALUES ('dummy@dummy.com', 'dummy', 1, 'dummy');
+VALUES ('dummy@dummy.com', 'dummy', 0, 'dummy');
 
-INSERT INTO tema (nombretema, color, correo)
-VALUES ('dummytema', 'azul', 'dummy@dummy.com');
+INSERT INTO tema (nombretema, r, g, b, correo)
+VALUES ('dummytema', 0, 0, 0, 'dummy@dummy.com');
 
 INSERT INTO marcador (descripcion, latitud, longitud, idtema)
 VALUES ('St. Petersburg', 59.89, 30.26, 1);
